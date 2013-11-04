9d516dc11c6e79511c7b7e5429ee19ca0600187b
Annebeth Buis <annebethbuis@hotmail.com>
2013-11-04 16:15:04 +0100

    The "Gooi!" and "Opnieuw" buttons in "Eenvoudige zinnen" now work correctly.

--- js/nt2-taalspel.js
+++ js/nt2-taalspel.js
@@ -1,11 +1,3 @@
-//$().ready(function() {
-//            alert("hello world");
-//        });
-
-//$(document).on('pagebeforechange', '#page1', function() {
-//	alert("hello world!";)
-//});
-
 $(document).on('pagebeforeshow','#page1' ,function(e,data){    
     $(document).on('click', '#eenvoudig-click',function(e) {
     	var words = ['hebben', 'zijn', 'gaan', 'doen', 'luisteren', 'wonen', 'herhalen', 'maken']
@@ -21,6 +13,22 @@ $(document).on('pagebeforeshow','#page1' ,function(e,data){
     	$('#dobbel-1').attr('src', randomDobbel_1);
     	$('#dobbel-2').attr('src', randomDobbel_2);
     	$('#dobbel-3').attr('src', randomDobbel_3);
-    });    
-});
+    });
+    $(document).on('click', '#eenvoudig-new-click',function(e) {
+    	var words = ['hebben', 'zijn', 'gaan', 'doen', 'luisteren', 'wonen', 'herhalen', 'maken']
+    	var randomWord = words[Math.floor(Math.random()*words.length)];
+    	$('#kaart-tekst').text(randomWord);
+    });
+    $(document).on('click', '#eenvoudig-roll-click',function(e) {
+    	var pvnImgs = ['img/ik.png', 'img/jij.png', 'img/hijzijhet.png', 'img/u.png', 'img/wijjulliezij.png', 'img/joker.png']
+    	var tijdImgs = ['img/PRF.png', 'img/PRE.png', 'img/IMP.png']
+    	var zinImgs = ['img/H.png', 'img/HI.png', 'img/IV.png', 'img/V.png']
 
+    	var randomDobbel_1 = pvnImgs[Math.floor(Math.random()*pvnImgs.length)];
+    	var randomDobbel_2 = tijdImgs[Math.floor(Math.random()*tijdImgs.length)];
+    	var randomDobbel_3 = zinImgs[Math.floor(Math.random()*zinImgs.length)];
+    	$('#dobbel-1').attr('src', randomDobbel_1);
+    	$('#dobbel-2').attr('src', randomDobbel_2);
+    	$('#dobbel-3').attr('src', randomDobbel_3);
+    });
+});
