19125666cf1d045ee9f4f88c792f4668cc4dd5a2
Annebeth Buis <annebethbuis@hotmail.com>
2013-11-04 16:39:34 +0100

    Added randomization to "Gevorderden" > "Werkwoorden".

--- js/nt2-taalspel.js
+++ js/nt2-taalspel.js
@@ -10,14 +10,14 @@ $(document).on('pagebeforeshow','#page1' ,function(e,data){
     	var randomDobbel_2 = tijdImgs[Math.floor(Math.random()*tijdImgs.length)];
     	var randomDobbel_3 = zinImgs[Math.floor(Math.random()*zinImgs.length)];
     	$('#kaart-tekst').text(randomWord);
-    	$('#dobbel-1').attr('src', randomDobbel_1);
-    	$('#dobbel-2').attr('src', randomDobbel_2);
-    	$('#dobbel-3').attr('src', randomDobbel_3);
+    	$('#dobbel-1-ev').attr('src', randomDobbel_1);
+    	$('#dobbel-2-ev').attr('src', randomDobbel_2);
+    	$('#dobbel-3-ev').attr('src', randomDobbel_3);
     });
     $(document).on('click', '#eenvoudig-new-click',function(e) {
     	var words = ['hebben', 'zijn', 'gaan', 'doen', 'luisteren', 'wonen', 'herhalen', 'maken']
     	var randomWord = words[Math.floor(Math.random()*words.length)];
-    	$('#kaart-tekst').text(randomWord);
+    	$('#kaart-tekst-ev').text(randomWord);
     });
     $(document).on('click', '#eenvoudig-roll-click',function(e) {
     	var pvnImgs = ['img/ik.png', 'img/jij.png', 'img/hijzijhet.png', 'img/u.png', 'img/wijjulliezij.png', 'img/joker.png']
@@ -27,8 +27,23 @@ $(document).on('pagebeforeshow','#page1' ,function(e,data){
     	var randomDobbel_1 = pvnImgs[Math.floor(Math.random()*pvnImgs.length)];
     	var randomDobbel_2 = tijdImgs[Math.floor(Math.random()*tijdImgs.length)];
     	var randomDobbel_3 = zinImgs[Math.floor(Math.random()*zinImgs.length)];
-    	$('#dobbel-1').attr('src', randomDobbel_1);
-    	$('#dobbel-2').attr('src', randomDobbel_2);
-    	$('#dobbel-3').attr('src', randomDobbel_3);
+    	$('#dobbel-1-ev').attr('src', randomDobbel_1);
+    	$('#dobbel-2-ev').attr('src', randomDobbel_2);
+    	$('#dobbel-3-ev').attr('src', randomDobbel_3);
+    });
+    $(document).on('click', '#ww-click',function(e) {
+        var words = ['vinden','weten','durven','streven','veranderen']
+        var pvnImgs = ['img/ik.png', 'img/jij.png', 'img/hijzijhet.png', 'img/u.png', 'img/wijjulliezij.png', 'img/joker.png']
+        var tijdImgs = ['img/PRF.png', 'img/PRE.png', 'img/IMP.png']
+        var zinImgs = ['img/H.png', 'img/HI.png', 'img/IV.png', 'img/V.png']
+        var randomWord = words[Math.floor(Math.random()*words.length)];
+        var randomDobbel_1 = pvnImgs[Math.floor(Math.random()*pvnImgs.length)];
+        var randomDobbel_2 = tijdImgs[Math.floor(Math.random()*tijdImgs.length)];
+        var randomDobbel_3 = zinImgs[Math.floor(Math.random()*zinImgs.length)];
+
+        $('#kaart-tekst-ww').text(randomWord);
+        $('#dobbel-1-ww').attr('src', randomDobbel_1);
+        $('#dobbel-2-ww').attr('src', randomDobbel_2);
+        $('#dobbel-3-ww').attr('src', randomDobbel_3);
     });
 });
