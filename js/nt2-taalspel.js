bb50d7b1e081e1af69d32005faa8ef98fca060fe
Annebeth Buis <annebethbuis@hotmail.com>
2013-11-04 15:26:54 +0100

    "Eenvoudige zinnen" shows random word when opened from Menu.

--- js/nt2-taalspel.js
+++ js/nt2-taalspel.js
@@ -1,3 +1,16 @@
-$(document).ready(function() {
-            alert("hello world");
-        });
+//$().ready(function() {
+//            alert("hello world");
+//        });
+
+//$(document).on('pagebeforechange', '#page1', function() {
+//	alert("hello world!";)
+//});
+
+$(document).on('pagebeforeshow','#page1' ,function(e,data){    
+    $(document).on('click', '#eenvoudig-click',function(e) {
+    	var words = ['hebben', 'zijn', 'gaan', 'doen', 'luisteren', 'wonen', 'herhalen', 'maken']
+    	var randomword = words[Math.floor(Math.random()*words.length)];
+    	$('#kaart-tekst').text(randomword);
+    });    
+});
+
