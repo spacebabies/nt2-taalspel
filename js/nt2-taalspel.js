35934b9f5dd70095e03b4b2b28c887b9529e7b2e
Annebeth Buis <annebethbuis@hotmail.com>
2013-11-04 15:59:05 +0100

    "Eenvoudige zinnen" now has changing dice and word cards.

--- js/nt2-taalspel.js
+++ js/nt2-taalspel.js
@@ -9,8 +9,18 @@
 $(document).on('pagebeforeshow','#page1' ,function(e,data){    
     $(document).on('click', '#eenvoudig-click',function(e) {
     	var words = ['hebben', 'zijn', 'gaan', 'doen', 'luisteren', 'wonen', 'herhalen', 'maken']
-    	var randomword = words[Math.floor(Math.random()*words.length)];
-    	$('#kaart-tekst').text(randomword);
+    	var pvnImgs = ['img/ik.png', 'img/jij.png', 'img/hijzijhet.png', 'img/u.png', 'img/wijjulliezij.png', 'img/joker.png']
+    	var tijdImgs = ['img/PRF.png', 'img/PRE.png', 'img/IMP.png']
+    	var zinImgs = ['img/H.png', 'img/HI.png', 'img/IV.png', 'img/V.png']
+
+    	var randomWord = words[Math.floor(Math.random()*words.length)];
+    	var randomDobbel_1 = pvnImgs[Math.floor(Math.random()*pvnImgs.length)];
+    	var randomDobbel_2 = tijdImgs[Math.floor(Math.random()*tijdImgs.length)];
+    	var randomDobbel_3 = zinImgs[Math.floor(Math.random()*zinImgs.length)];
+    	$('#kaart-tekst').text(randomWord);
+    	$('#dobbel-1').attr('src', randomDobbel_1);
+    	$('#dobbel-2').attr('src', randomDobbel_2);
+    	$('#dobbel-3').attr('src', randomDobbel_3);
     });    
 });
 
