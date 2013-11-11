75a44254609daa4425ee4dd218d5cf23a5383ea5
Annebeth Buis <annebethbuis@hotmail.com>
2013-11-11 15:14:32 +0100

    Cube parked in Cube-branch and Eenvoudige zinnen is working in the old style again.

--- js/nt2-taalspel.js
+++ js/nt2-taalspel.js
@@ -1,30 +1,41 @@
-//Dit deel geldt voor het deel Eenvoudige zinnen van het menu.
 $(document).on('pagebeforeshow','#page1' ,function(e,data){
-    $(document).on('click', '#eenvoudig-click',function(e) {
-        var currentOption = $('#cube').attr('class');
-        var options = ['show-front','show-back', 'show-left', 'show-right', 'show-top', 'show-bottom'];
-        var randomOption = options[Math.floor(Math.random()*options.length)];
-        $('#cube').removeClass(currentOption).addClass(randomOption);
-
-        var words = ['hebben', 'zijn', 'gaan', 'doen', 'luisteren', 'wonen', 'herhalen', 'maken']
+//Dit deel geldt voor het deel Eenvoudige zinnen van het menu.
+   $(document).on('click', '#eenvoudig-click',function(e) {
+        var words = ['hebben', 'zijn', 'gaan', 'doen', 'luisteren', 'wonen', 'herhalen','maken']
+        var pvnImgs = ['img/ik.png', 'img/jij.png', 'img/hijzijhet.png', 'img/u.png', 'img/wijjulliezij.png', 'img/joker.png']
+        var tijdImgs = ['img/PRF.png', 'img/PRE.png', 'img/IMP.png']
+        var zinImgs = ['img/H.png', 'img/HI.png', 'img/IV.png', 'img/V.png']
         var randomWord = words[Math.floor(Math.random()*words.length)];
+        var randomDobbel_1 = pvnImgs[Math.floor(Math.random()*pvnImgs.length)];
+        var randomDobbel_2 = tijdImgs[Math.floor(Math.random()*tijdImgs.length)];
+        var randomDobbel_3 = zinImgs[Math.floor(Math.random()*zinImgs.length)];
+
         $('#kaart-tekst-ev').text(randomWord);
+        $('#dobbel-1-ev').attr('src', randomDobbel_1);
+        $('#dobbel-2-ev').attr('src', randomDobbel_2);
+        $('#dobbel-3-ev').attr('src', randomDobbel_3);
     });
 
     $(document).on('click', '#eenvoudig-new-click',function(e) {
-        var words = ['hebben', 'zijn', 'gaan', 'doen', 'luisteren', 'wonen', 'herhalen', 'maken']
+        var words = ['hebben', 'zijn', 'gaan', 'doen', 'luisteren', 'wonen', 'herhalen','maken']
         var randomWord = words[Math.floor(Math.random()*words.length)];
+
         $('#kaart-tekst-ev').text(randomWord);
     });
 
     $(document).on('click', '#eenvoudig-roll-click',function(e) {
-        $('#cube').each(function(){
-            var currentOption = $(this).attr('class');
-            var options = ['show-front','show-back', 'show-left', 'show-right', 'show-top', 'show-bottom'];
-            var randomOption = options[Math.floor(Math.random()*options.length)];
-            $(this).removeClass(currentOption).addClass(randomOption);
-        });
+        var pvnImgs = ['img/ik.png', 'img/jij.png', 'img/hijzijhet.png', 'img/u.png', 'img/wijjulliezij.png', 'img/joker.png']
+        var tijdImgs = ['img/PRF.png', 'img/PRE.png', 'img/IMP.png']
+        var zinImgs = ['img/H.png', 'img/HI.png', 'img/IV.png', 'img/V.png']
+        var randomDobbel_1 = pvnImgs[Math.floor(Math.random()*pvnImgs.length)];
+        var randomDobbel_2 = tijdImgs[Math.floor(Math.random()*tijdImgs.length)];
+        var randomDobbel_3 = zinImgs[Math.floor(Math.random()*zinImgs.length)];
+
+        $('#dobbel-1-ev').attr('src', randomDobbel_1);
+        $('#dobbel-2-ev').attr('src', randomDobbel_2);
+        $('#dobbel-3-ev').attr('src', randomDobbel_3);
     });
+
 // Dit deel gaat over het menudeel Gevorderden > Conjuncties.
     $(document).on('click', '#conjuncties-click',function(e) {
         var words = ['maken', 'doen', 'vergeten', 'hebben', 'zijn']
