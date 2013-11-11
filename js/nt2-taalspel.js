4f90e24841ef1d76fe3ab875824d4c3bbfc015ce
Annebeth Buis <annebethbuis@hotmail.com>
2013-11-11 13:46:19 +0100

    Implemented all clicks for Thema's (both Seizoenen and Wetenschap).

--- js/nt2-taalspel.js
+++ js/nt2-taalspel.js
@@ -25,7 +25,71 @@ $(document).on('pagebeforeshow','#page1' ,function(e,data){
             $(this).removeClass(currentOption).addClass(randomOption);
         });
     });
+
+
+// Dit deel gaat over het menudeel Thema's > Seizoenen..
+    $(document).on('click', '#season-click',function(e) {
+        var words = ['de paraplu', 'het terrasje', 'warm', 'de winterjas', 'buiten']
+        var pvnImgs = ['img/ik.png', 'img/jij.png', 'img/hijzijhet.png', 'img/u.png', 'img/wijjulliezij.png', 'img/joker.png']
+        var tijdImgs = ['img/PRF.png', 'img/PRE.png', 'img/IMP.png']
+        var zinImgs = ['img/H.png', 'img/HI.png', 'img/IV.png', 'img/V.png']
+        var randomWord = words[Math.floor(Math.random()*words.length)];
+        var randomDobbel_1 = pvnImgs[Math.floor(Math.random()*pvnImgs.length)];
+        var randomDobbel_2 = tijdImgs[Math.floor(Math.random()*tijdImgs.length)];
+        var randomDobbel_3 = zinImgs[Math.floor(Math.random()*zinImgs.length)];
+
+        $('#kaart-tekst-season').text(randomWord);
+        $('#dobbel-1-season').attr('src', randomDobbel_1);
+        $('#dobbel-2-season').attr('src', randomDobbel_2);
+        $('#dobbel-3-season').attr('src', randomDobbel_3);
+    });
+
+    $(document).on('click', '#season-new-click',function(e) {
+        var words = ['de paraplu', 'het terrasje', 'warm', 'de winterjas', 'buiten']
+        var randomWord = words[Math.floor(Math.random()*words.length)];
+
+        $('#kaart-tekst-season').text(randomWord);
+    });
+
+    $(document).on('click', '#season-roll-click',function(e) {
+        var pvnImgs = ['img/ik.png', 'img/jij.png', 'img/hijzijhet.png', 'img/u.png', 'img/wijjulliezij.png', 'img/joker.png']
+        var tijdImgs = ['img/PRF.png', 'img/PRE.png', 'img/IMP.png']
+        var zinImgs = ['img/H.png', 'img/HI.png', 'img/IV.png', 'img/V.png']
+        var randomDobbel_1 = pvnImgs[Math.floor(Math.random()*pvnImgs.length)];
+        var randomDobbel_2 = tijdImgs[Math.floor(Math.random()*tijdImgs.length)];
+        var randomDobbel_3 = zinImgs[Math.floor(Math.random()*zinImgs.length)];
+
+        $('#dobbel-1-season').attr('src', randomDobbel_1);
+        $('#dobbel-2-season').attr('src', randomDobbel_2);
+        $('#dobbel-3-season').attr('src', randomDobbel_3);
+    });
+
+// Dit deel gaat over het menudeel Thema's > Wetenschap.
+    $(document).on('click', '#science-click',function(e) {
+        var words = ['het onderzoek', 'de oorzaak', 'de grafiek', 'afnemen', 'blijken']
+        var tijdImgs = ['img/PRF.png', 'img/PRE.png', 'img/IMP.png']
+        var randomWord = words[Math.floor(Math.random()*words.length)];
+        var randomDobbel = tijdImgs[Math.floor(Math.random()*tijdImgs.length)];
+
+        $('#kaart-tekst-science').text(randomWord);
+        $('#dobbel-science').attr('src', randomDobbel);
+    });
+
+    $(document).on('click', '#science-new-click',function(e) {
+        var words = ['het onderzoek', 'de oorzaak', 'de grafiek', 'afnemen', 'blijken']
+        var randomWord = words[Math.floor(Math.random()*words.length)];
+
+        $('#kaart-tekst-science').text(randomWord);
+    });
+
+    $(document).on('click', '#science-roll-click',function(e) {
+        var tijdImgs = ['img/PRF.png', 'img/PRE.png', 'img/IMP.png']
+        var randomDobbel = tijdImgs[Math.floor(Math.random()*tijdImgs.length)];
+
+        $('#dobbel-science').attr('src', randomDobbel);
+    });
 });
+
 // // Dit deel gaat over het menudeel Gevorderden > Werkwoorden.
 //     $(document).on('click', '#ww-click',function(e) {
 //         var words = ['vinden','weten','durven','streven','veranderen']
