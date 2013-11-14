bfc45658780343680ec987b0dd198e762fb6103b
Annebeth Buis <annebethbuis@hotmail.com>
2013-11-14 23:31:56 +0100

    Took 4th image out of Gevorderden > Werkwoorden and made button's work.

--- www/js/nt2-taalspel.js
+++ www/js/nt2-taalspel.js
@@ -133,6 +133,45 @@ $(document).on('pagebeforeshow','#page1' ,function(e,data){
         }
     });
 
+// Dit deel gaat over het menudeel Gevorderden > Werkwoorden.
+    $(document).on('click', '#ww-click',function(e) {
+        var words = ['vinden','weten','durven','streven','veranderen']
+        var pvnImgs = ['img/ik.png', 'img/jij.png', 'img/hijzijhet.png', 'img/u.png', 'img/wijjulliezij.png', 'img/joker.png']
+        var tijdImgs = ['img/prf.png', 'img/PRE.png', 'img/IMP.png']
+        var zinImgs = ['img/H.png', 'img/hi.png', 'img/IV.png', 'img/V.png']
+        var randomWord = words[Math.floor(Math.random()*words.length)];
+        var randomDobbel_1 = pvnImgs[Math.floor(Math.random()*pvnImgs.length)];
+        var randomDobbel_2 = tijdImgs[Math.floor(Math.random()*tijdImgs.length)];
+        var randomDobbel_3 = zinImgs[Math.floor(Math.random()*zinImgs.length)];
+
+        $('#kaart-tekst-ww').text(randomWord);
+        $('#dobbel-1-ww').attr('src', randomDobbel_1);
+        $('#dobbel-2-ww').attr('src', randomDobbel_2);
+        $('#dobbel-3-ww').attr('src', randomDobbel_3);
+    });
+
+    $(document).on('click', '#ww-new-click',function(e) {
+        var words = ['vinden','weten','durven','streven','veranderen']
+        var voegwoordImgs = ['img/en.png', 'img/want.png', 'img/omdat.png', 'img/maar.png', 'img/hoewel.png']
+        var randomWord = words[Math.floor(Math.random()*words.length)];
+        var randomDobbel_4 = voegwoordImgs[Math.floor(Math.random()*voegwoordImgs.length)];
+
+        $('#kaart-tekst-ww').text(randomWord);
+    });
+
+    $(document).on('click', '#ww-roll-click',function(e) {
+        var pvnImgs = ['img/ik.png', 'img/jij.png', 'img/hijzijhet.png', 'img/u.png', 'img/wijjulliezij.png', 'img/joker.png']
+        var tijdImgs = ['img/prf.png', 'img/PRE.png', 'img/IMP.png']
+        var zinImgs = ['img/H.png', 'img/hi.png', 'img/IV.png', 'img/V.png']
+        var randomDobbel_1 = pvnImgs[Math.floor(Math.random()*pvnImgs.length)];
+        var randomDobbel_2 = tijdImgs[Math.floor(Math.random()*tijdImgs.length)];
+        var randomDobbel_3 = zinImgs[Math.floor(Math.random()*zinImgs.length)];
+
+        $('#dobbel-1-ww').attr('src', randomDobbel_1);
+        $('#dobbel-2-ww').attr('src', randomDobbel_2);
+        $('#dobbel-3-ww').attr('src', randomDobbel_3);
+    });
+
 // Dit deel gaat over het menudeel Gevorderden > Conjuncties.
     $(document).on('click', '#conjuncties-click',function(e) {
         var classState = $('#kaart-extra').attr('class')
@@ -267,53 +306,3 @@ $(document).on('pagebeforeshow','#page1' ,function(e,data){
         $('#dobbel-science').attr('src', randomDobbel);
     });
 });
-
-// // Dit deel gaat over het menudeel Gevorderden > Werkwoorden.
-//     $(document).on('click', '#ww-click',function(e) {
-//         var words = ['vinden','weten','durven','streven','veranderen']
-//         var pvnImgs = ['img/ik.png', 'img/jij.png', 'img/hijzijhet.png', 'img/u.png', 'img/wijjulliezij.png', 'img/joker.png']
-//         var tijdImgs = ['img/prf.png', 'img/PRE.png', 'img/IMP.png']
-//         var zinImgs = ['img/H.png', 'img/hi.png', 'img/IV.png', 'img/V.png']
-//         var randomWord = words[Math.floor(Math.random()*words.length)];
-//         var randomDobbel_1 = pvnImgs[Math.floor(Math.random()*pvnImgs.length)];
-//         var randomDobbel_2 = tijdImgs[Math.floor(Math.random()*tijdImgs.length)];
-//         var randomDobbel_3 = zinImgs[Math.floor(Math.random()*zinImgs.length)];
-
-//         $('#kaart-tekst-ww').text(randomWord);
-//         $('#dobbel-1-ww').attr('src', randomDobbel_1);
-//         $('#dobbel-2-ww').attr('src', randomDobbel_2);
-//         $('#dobbel-3-ww').attr('src', randomDobbel_3);
-//         $('#dobbel-4-ww').attr('style', "width: 60px; height: 60px; opacity: 0.0");
-//     });
-//     $(document).on('click', '#ww-new-click',function(e) {
-//         var words = ['vinden','weten','durven','streven','veranderen']
-//         var voegwoordImgs = ['img/en.png', 'img/want.png', 'img/omdat.png', 'img/maar.png', 'img/hoewel.png']
-//         var randomWord = words[Math.floor(Math.random()*words.length)];
-//         var randomDobbel_4 = voegwoordImgs[Math.floor(Math.random()*voegwoordImgs.length)];
-
-//         $('#kaart-tekst-ww').text(randomWord);
-//         $('#dobbel-4-ww').attr('src', randomDobbel_4);
-//         $('#dobbel-4-ww').attr('style', "width: 60px; height: 60px;");
-//     });
-//     $(document).on('click', '#ww-roll-click',function(e) {
-//         var pvnImgs = ['img/ik.png', 'img/jij.png', 'img/hijzijhet.png', 'img/u.png', 'img/wijjulliezij.png', 'img/joker.png']
-//         var tijdImgs = ['img/prf.png', 'img/PRE.png', 'img/IMP.png']
-//         var zinImgs = ['img/H.png', 'img/hi.png', 'img/IV.png', 'img/V.png']
-//         var voegwoordImgs = ['img/en.png', 'img/want.png', 'img/omdat.png', 'img/maar.png', 'img/hoewel.png']
-//         var randomDobbel_1 = pvnImgs[Math.floor(Math.random()*pvnImgs.length)];
-//         var randomDobbel_2 = tijdImgs[Math.floor(Math.random()*tijdImgs.length)];
-//         var randomDobbel_3 = zinImgs[Math.floor(Math.random()*zinImgs.length)];
-//         var randomDobbel_4 = voegwoordImgs[Math.floor(Math.random()*voegwoordImgs.length)];
-
-//         $('#dobbel-1-ww').attr('src', randomDobbel_1);
-//         $('#dobbel-2-ww').attr('src', randomDobbel_2);
-//         $('#dobbel-3-ww').attr('src', randomDobbel_3);
-//         $('#dobbel-4-ww').attr('src', randomDobbel_4);
-//         $('#dobbel-4-ww').attr('style', "width: 60px; height: 60px;");
-//     });
-// });
-// // Dit deel van het menu is voor Gevorderden > Conjuncties.
-
-
-
-
